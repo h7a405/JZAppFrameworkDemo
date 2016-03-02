f22364512ca77821fae1bfaf7b28f78f25a28473
JasonChengzi <3923240@qq.com>
2016-03-02 09:17:35 +0800

    framework updates

---
+++ JZAppFrameworkDemo/Helper/System/TimerHelper.swift
@@ -0,0 +1,13 @@
+//
+//  TimerHelper.swift
+//  JZAppFrameworkDemo
+//
+//  Created by Jason.Chengzi on 16/02/29.
+//  Copyright © 2016年 weSwift. All rights reserved.
+//
+
+import UIKit
+
+class TimerHelper: NSObject {
+
+}
